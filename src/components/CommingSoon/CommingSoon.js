059ca68f7aee2da45e8b70a38d423a8bd47bf860
Michał Świderski <michal.swiderski@cyberain.pl>
2022-03-12 12:54:14 +0100

    Aktualizacja CommingSoon

--- src/components/CommingSoon/CommingSoon.js
+++ src/components/CommingSoon/CommingSoon.js
@@ -3,6 +3,11 @@ import styles from './CommingSoon.module.scss';
 import logoImage from '../../assets/images/logo_michal_swiderski.png';
 import { FaLinkedin, FaFacebookSquare, FaMailBulk, FaGoogleDrive } from 'react-icons/fa'
 
+
+const current = new Date();
+const date = `${current.getFullYear()}`;
+
+
 const CommingSoon = () => (
     <div className={styles.wrapper}>
         <div className={styles.logo}>
@@ -11,29 +16,28 @@ const CommingSoon = () => (
             </a>
         </div>
         <div className={styles.text}>
-            <h2>Strona w budowie</h2>
-            <p>Dobry czas na reorganizację.</p>
+           
         </div>
         <div className={styles.social}>
-            <a href="https://www.linkedin.com/in/michal-swiderski/" target="_blank">
+            <a href="https://www.linkedin.com/in/michal-swiderski/" target="_blank" rel="noreferrer">
                 <FaLinkedin className={styles.Icon}/>
             </a>
-            <a href="https://www.facebook.com/michal.michal.happy/" target="_blank">
+            <a href="https://www.facebook.com/michal.michal.happy/" target="_blank" rel="noreferrer">
                 <FaFacebookSquare className={styles.Icon}/>
             </a>
 
             <span className={styles.border}></span>
 
-            <a href="http://poczta.michal-swiderski.pl" target="_blank">
+            <a href="http://poczta.michal-swiderski.pl" target="_blank" rel="noreferrer">
                 <FaMailBulk className={styles.Icon}/>
             </a>
-            <a href="http://dysk.michal-swiderski.pl" target="_blank">
+            <a href="http://dysk.michal-swiderski.pl" target="_blank" rel="noreferrer">
                 <FaGoogleDrive className={styles.Icon}/>
             </a>
 
         </div>
         <div className={styles.copyright}>
-            Michał Świderski ©2021
+            Michał Świderski ©2013 - {date}
         </div>
     </div>
 );
